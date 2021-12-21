82debd75305ba89fcd8850adb9977813d714344f
overkam <superivan0755@gmail.com>
2021-12-21 16:50:33 +0300

    fixed tags

--- src/hooks/useFetchTags.ts
+++ src/hooks/useFetchTags.ts
@@ -6,10 +6,8 @@ import { iconAllNFTs } from 'assets/img';
 const useFetchTags = () => {
   const fetchTags = useCallback(async () => {
     try {
-      const {
-        data: { tags },
-      } = await storeApi.getTags();
-      const fullTags = [{ title: 'All NFTs', icon: iconAllNFTs }, ...tags];
+      const { data } = await storeApi.getTags();
+      const fullTags = [{ title: 'All NFTs', icon: iconAllNFTs }, ...data];
       rootStore.nftTags.setTags(fullTags);
     } catch (error) {
       console.log(error);
