e63913032beab274e525e987f03f937198de16e5
overkam <superivan0755@gmail.com>
2022-01-12 14:07:22 +0300

    fixed rates

--- src/services/api/rates.ts
+++ src/services/api/rates.ts
@@ -1,5 +1,5 @@
 import axios from '../../core/axios';
 
 export default {
-  getRates: (network?: string) => axios.get(`/rates/?network=${network || localStorage.lessnft_nft_chainName}`),
+  getRates: () => axios.get(`/rates/?network=${localStorage.lessnft_nft_chainName}`),
 };
