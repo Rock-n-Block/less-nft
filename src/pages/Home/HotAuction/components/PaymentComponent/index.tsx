6f743b290fc69919481ef007899dd253284f5b3a
overkam <superivan0755@gmail.com>
2022-01-28 17:16:50 +0300

    added days and start-end timer

--- src/pages/Home/HotAuction/components/PaymentComponent/index.tsx
+++ src/pages/Home/HotAuction/components/PaymentComponent/index.tsx
@@ -8,7 +8,7 @@ import { observer } from 'mobx-react-lite';
 import { storeApi } from 'services/api';
 import { useWalletConnectorContext } from 'services/walletConnect';
 import { useMst } from 'store';
-import { chainsEnum, INft, TNullable } from 'typings';
+import { INft, TNullable } from 'typings';
 
 import styles from './styles.module.scss';
 import { toast } from 'react-toastify';
@@ -42,10 +42,11 @@ const PaymentComponent: FC<Props> = observer(
     const [isApproved, setApproved] = React.useState<boolean>(false);
     const [isApproving, setApproving] = React.useState<boolean>(false);
     const [time, setTime] = useState<any>();
+    const [days, setDays] = useState(0);
     const [isEndingAuction, setIsEndingAuction] = useState(false);
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const ExchangeAddress = exchangeAddrs[localStorage.nftcrowd_nft_chainName as chainsEnum];
+    const ExchangeAddress = exchangeAddrs[localStorage.lessnft_nft_chainName as chainsEnum];
 
     const currentPrice = React.useMemo(() => {
       if (nft) {
@@ -81,54 +82,30 @@ const PaymentComponent: FC<Props> = observer(
           setApproved(true);
           return;
         }
-        if (localStorage.nftcrowd_nft_chainName === chainsEnum.Tron) {
-          walletService
-            .trxCheckAllowance(nft.currency.symbol.toUpperCase(), ExchangeAddress, user.address)
-            .then((res: boolean) => {
-              setApproved(res);
-            })
-            .catch((err: any) => {
-              setApproved(false);
-              console.error(err, 'check');
-            });
-        } else {
-          walletService
-            .checkTokenAllowance(nft.currency.symbol.toUpperCase(), 18, ExchangeAddress)
-            .then((res: boolean) => {
-              setApproved(res);
-            })
-            .catch((err: any) => {
-              setApproved(false);
-              console.error(err, 'check');
-            });
-        }
+        walletService
+          .checkTokenAllowance(nft.currency.symbol.toUpperCase(), 18, ExchangeAddress)
+          .then((res: boolean) => {
+            setApproved(res);
+          })
+          .catch((err: any) => {
+            setApproved(false);
+            console.error(err, 'check');
+          });
       }
-    }, [nft, walletService, ExchangeAddress, user.address]);
+    }, [nft, walletService, ExchangeAddress]);
 
     const handleApproveToken = React.useCallback(() => {
       if (nft) {
         setApproving(true);
-        if (localStorage.nftcrowd_nft_chainName === chainsEnum.Tron) {
-          walletService
-            .trxApproveToken(nft.currency.symbol.toUpperCase(), ExchangeAddress)
-            .then(() => {
-              setApproved(true);
-            })
-            .catch((err: any) => {
-              console.error(err, 'err approve');
-            })
-            .finally(() => setApproving(false));
-        } else {
-          walletService
-            .approveToken(nft.currency.symbol.toUpperCase(), 18, ExchangeAddress)
-            .then(() => {
-              setApproved(true);
-            })
-            .catch((err: any) => {
-              console.error(err, 'err approve');
-            })
-            .finally(() => setApproving(false));
-        }
+        walletService
+          .approveToken(nft.currency.symbol.toUpperCase(), 18, ExchangeAddress)
+          .then(() => {
+            setApproved(true);
+          })
+          .catch((err: any) => {
+            console.error(err, 'err approve');
+          })
+          .finally(() => setApproving(false));
       }
     }, [ExchangeAddress, nft, walletService]);
 
@@ -250,6 +227,7 @@ const PaymentComponent: FC<Props> = observer(
         const interval = 1000;
         timeInterval = setInterval(() => {
           duration = moment.duration(+duration - interval, 'milliseconds');
+          setDays(Math.trunc(duration.asDays()));
           setTime(moment(duration.asMilliseconds()));
         }, interval);
       }
@@ -257,8 +235,71 @@ const PaymentComponent: FC<Props> = observer(
       return () => clearInterval(timeInterval);
     }, [nft]);
 
+    useEffect(() => {
+      if (
+        days <= 0 &&
+        moment(time).hours() === 0 &&
+        moment(time).minutes() === 0 &&
+        moment(time).seconds() === 0 &&
+        onUpdateNft
+      ) {
+        setTimeout(() => {
+          onUpdateNft();
+        }, 500);
+      };
+    }, [days, onUpdateNft, time]);
+
     return (
       <div className={cx(className, { [styles.paymentSell]: nftSellingType === 'sell' })}>
+        {nft &&
+          nft?.start_auction &&
+          nft?.end_auction &&
+          (nft?.is_timed_auc_selling ? (
+            <div className={styles.right}>
+              <Text size="m" className={styles.rightTitle}>
+                Sale ends at {moment(nft.end_auction, 'X').format('MMMM Do YYYY, h:mm a')}
+              </Text>
+              <div className={styles.rightTimes}>
+                {days >= 1 ? (
+                  <div className={styles.rightTimesItem}>
+                    <Text size="xl" weight="bold" className={styles.rightTimes}>
+                      {days}
+                    </Text>
+                    <Text>Days</Text>
+                  </div>
+                ) : (
+                  <></>
+                )}
+                <div className={styles.rightTimesItem}>
+                  <Text size="xl" weight="bold" className={styles.rightTimes}>
+                    {moment(time).format('HH')}
+                  </Text>
+                  <Text>Hours</Text>
+                </div>
+                <div className={styles.rightTimesItem}>
+                  <Text size="xl" weight="bold" className={styles.rightTimes}>
+                    {moment(time).format('mm')}
+                  </Text>
+                  <Text>Minutes</Text>
+                </div>
+                <div className={styles.rightTimesItem}>
+                  <Text size="xl" weight="bold" className={styles.rightTimes}>
+                    {moment(time).format('ss')}
+                  </Text>
+                  <Text>Seconds</Text>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.right}>
+              <Text size="m" className={styles.rightTitle}>
+                Sale starts at {moment(nft.start_auction, 'X').format('MMMM Do YYYY, h:mm a')}
+              </Text>
+              <Text size="m" className={styles.rightTitle}>
+                Sale ends at {moment(nft.end_auction, 'X').format('MMMM Do YYYY, h:mm a')}
+              </Text>
+            </div>
+          ))}
         <div className={styles.left}>
           <div className={styles.priceWrapper}>
             <div>
@@ -280,7 +321,9 @@ const PaymentComponent: FC<Props> = observer(
 
           {user.address ? (
             <div className={styles.sellBtnsWrapper}>
-              {!isApproved && isUserCanApprove && (nft?.is_selling || nft?.is_auc_selling || nft?.is_timed_auc_selling) ? (
+              {!isApproved &&
+              isUserCanApprove &&
+              (nft?.is_selling || nft?.is_auc_selling || nft?.is_timed_auc_selling) ? (
                 <Button
                   padding="custom"
                   loading={isApproving}
@@ -325,33 +368,6 @@ const PaymentComponent: FC<Props> = observer(
             </div>
           ) : null}
         </div>
-        {nft && nft?.start_auction && nft?.end_auction && nft.is_timed_auc_selling && (
-          <div className={styles.right}>
-            <Text size="m" className={styles.rightTitle}>
-              Sale ends at {moment(nft.end_auction, 'X').format('MMMM Do YYYY, h:mm a')}
-            </Text>
-            <div className={styles.rightTimes}>
-              <div className={styles.rightTimesItem}>
-                <Text size="xl" weight="bold" className={styles.rightTimes}>
-                  {moment(time).format('HH')}
-                </Text>
-                <Text>Hours</Text>
-              </div>
-              <div className={styles.rightTimesItem}>
-                <Text size="xl" weight="bold" className={styles.rightTimes}>
-                  {moment(time).format('mm')}
-                </Text>
-                <Text>Minutes</Text>
-              </div>
-              <div className={styles.rightTimesItem}>
-                <Text size="xl" weight="bold" className={styles.rightTimes}>
-                  {moment(time).format('ss')}
-                </Text>
-                <Text>Seconds</Text>
-              </div>
-            </div>
-          </div>
-        )}
       </div>
     );
   },
